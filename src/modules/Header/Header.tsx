d5f9a8e937843f4f6a1b4ecb7104ccc091088d06
Степан Бухлов <stepanbuhlov@192.168.100.2>
2023-06-02 16:36:09 +0300

    fix bag

--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from "./Header.module.css";
-import logo from "../../assets/logo.svg";
+import logo from "../../assets/Union.svg";
+import text from "../../assets/Jobored.svg";
 import { ButtonsForHeader } from "./components/ButtonsForHeader/ButtonsForHeader";
 import { useNavigate } from "react-router-dom";
 
@@ -13,7 +14,11 @@ export const Header = () => {
 
   return (
     <div className={s.wrapper}>
-      <img className={s.logo} src={logo} onClick={toHome} />
+      {/* <img className={s.logo} src={logo} onClick={toHome} /> */}
+      <div className={s.logo} onClick={toHome}>
+        <img className={s.imageLogo} src={logo} />
+        <img className={s.textLogo} src={text} />
+      </div>
       <ButtonsForHeader />
     </div>
   );
